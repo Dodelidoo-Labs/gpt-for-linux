249e98824e6b6c0fdca909900208c9166f27eab9
Beda Schmid <beda@tukutoi.com>
2023-12-03 15:34:20 +0700

    Update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Check if the script is being run as root or with sudo
+if [ "$UID" -eq 0 ]; then
+    echo "Ugly! Do not run as sudo!"
+    exit 1
+fi
 
 # Define paths
 install_path="$HOME/.local/share/gpt4-search-app"
@@ -6,30 +11,50 @@ desktop_file_path="$HOME/.local/share/applications/gpt4-search-app.desktop"
 icon_path="$install_path/icon.png"
 script_path="$install_path/main.py"
 
-# Check and install dependencies
 check_dependencies() {
     dependencies=("python" "wget" "gtk4" "python-gobject" "python-requests" "python-secretstorage")
+    missing_dependencies=()
 
     for pkg in "${dependencies[@]}"; do
         if ! pacman -Qs "$pkg" > /dev/null; then
-            echo "Installing missing package: $pkg"
-            sudo pacman -S --noconfirm $pkg
+            missing_dependencies+=("$pkg")
         fi
     done
+
+    if [ ${#missing_dependencies[@]} -eq 0 ]; then
+        echo "All necessary dependencies are already installed."
+    else
+        echo "The following packages are missing and need to be installed: ${missing_dependencies[*]}"
+        read -p "Do you want to proceed with installation? (y/N) " response
+        case $response in
+            [yY][eE][sS]|[yY]) 
+                for pkg in "${missing_dependencies[@]}"; do
+                    echo "Installing $pkg..."
+                    sudo pacman -S $pkg
+                done
+                ;;
+            *)
+                echo "Installation cancelled by user."
+                exit 1
+                ;;
+        esac
+    fi
 }
 
 # Run the function
 check_dependencies
 
 # Create install directory
+echo "Creating installation directory..."
 mkdir -p "$install_path"
-
+echo "Downloading scripts..."
 # Download script, CSS, and icon
 # (Replace the URLs with actual URLs where your files are hosted)
-wget -O "$script_path" "http://example.com/main.py"
-wget -O "$icon_path" "http://example.com/icon.png"
-wget -O "$install_path/style.css" "http://example.com/style.css"
+wget -O "$script_path" "https://raw.githubusercontent.com/TukuToi/gpt-for-linux/main/main.py"
+wget -O "$icon_path" "https://raw.githubusercontent.com/TukuToi/gpt-for-linux/main/icon.png"
+wget -O "$install_path/style.css" "https://raw.githubusercontent.com/TukuToi/gpt-for-linux/main/style.css"
 
+echo "Creating Desktop file..."
 # Create .desktop file
 echo "[Desktop Entry]
 Type=Application
@@ -40,6 +65,7 @@ Terminal=false
 Categories=Utility;
 " > "$desktop_file_path"
 
+echo "Making scripts executable..."
 # Make the script executable
 chmod +x "$script_path"
 
